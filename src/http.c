54adcd4bfd8ef6f7ab53577165685c90a811bb1f
M Divyesh <mdivyesh893@gmail.com>
2026-09-24 09:38:23 +0530

    site name extraction

--- src/http.c
+++ src/http.c
@@ -11,3 +11,11 @@ static void trim(char *text) {
     size_t length = strlen(text);
     while (length && isspace((unsigned char)text[length - 1])) text[--length] = '\0';
 }
+static int copy_part(char *dst, size_t cap, const char *src, size_t n) 
+{ 
+  if (!n || n >= cap)
+    return -1; 
+  memcpy(dst, src, n);
+  dst[n] = '\0'; 
+  return 0; 
+}
